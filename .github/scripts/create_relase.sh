83e8f36a0c2aa4f60308d0b56889a76546370aa4
pioka <20146647+pioka@users.noreply.github.com>
2026-05-30 22:44:53 +0900

    ci: workflow_dispatchによるリリース作成に対応

---
+++ .github/scripts/create_relase.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+
+PACKAGE_NAME="$(jq -r '.name' package.json)"
+PACKAGE_VERSION="$(jq -r '.version' package.json)"
+PACKAGE_ZIP="${PACKAGE_NAME}-${PACKAGE_VERSION}.zip"
+GIT_TAG="v${PACKAGE_VERSION}"
+
+# check tag exists
+if git ls-remote --tags origin | cut -f2 | grep -qF "refs/tags/$GIT_TAG"; then
+  echo "ERROR: tag:$GIT_TAG already exists on repository. Update version on package.json."
+  exit 1
+fi
+
+# push tag
+git tag "$GIT_TAG"
+git push origin "$GIT_TAG"
+
+# create Release
+zip -x '.*' -r "$PACKAGE_ZIP" .
+gh release create --generate-notes "$GIT_TAG" package.json "$PACKAGE_ZIP"
