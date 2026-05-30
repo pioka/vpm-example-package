6e7daabfb252b71475f7dfe205b87a81dbf4d99e
pioka <20146647+pioka@users.noreply.github.com>
2026-05-30 13:10:14 +0900

    サンプルとしての最低限の実装だけ追加

---
+++ Editor/ExampleTool.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace YourUserNameHere.ExampleTool
+{
+    internal static class ExampleTool
+    {
+        [MenuItem("Tools/ExampleTool")]
+        private static void Execute()
+        {
+            Debug.Log("ExampleTool");
+        }
+    }
+}
