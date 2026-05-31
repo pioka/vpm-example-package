1f9bb35194e45889fc0b1e605320555b95428789
pioka <20146647+pioka@users.noreply.github.com>
2026-05-31 16:15:48 +0900

    サンプルの内容を修正

---
+++ Editor/VpmExamplePackage.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Pioka.VpmExamplePackage
+{
+    internal static class VpmExamplePackage
+    {
+        [MenuItem("Tools/VpmExamplePackage")]
+        private static void Execute()
+        {
+            Debug.Log("VpmExamplePackage");
+        }
+    }
+}
